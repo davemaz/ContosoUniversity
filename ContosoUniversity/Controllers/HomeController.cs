bcaf493ee43cc363ec1eb4b731c974e16023792a
David Mazmanian <DavidMazmanian@hotmail.com>
2016-08-23 09:20:02 -0400

    Added student statistics view model.

--- ContosoUniversity/Controllers/HomeController.cs
+++ ContosoUniversity/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ContosoUniversity.DAL;
+using ContosoUniversity.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +10,7 @@ namespace ContosoUniversity.Controllers
 {
     public class HomeController : Controller
     {
+        public SchoolContext db = new SchoolContext();
         public ActionResult Index()
         {
             return View();
@@ -15,9 +18,20 @@ namespace ContosoUniversity.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            IQueryable<EnrollmentDateGroup> data = from student in db.Students
+                group student by student.EnrollmentDate into dateGroup
+                select new EnrollmentDateGroup()
+                {
+                    EnrollmentDate = dateGroup.Key,
+                    StudentCount = dateGroup.Count()
+                };
+            return View(data.ToList());
+        }
 
-            return View();
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
         }
 
         public ActionResult Contact()
